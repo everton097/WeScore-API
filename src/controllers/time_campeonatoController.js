f4d23fc98eec2f29eb372a77f57e5b0564e241d3
Everton F Silva <everton097v1@outlook.com>
2023-10-20 20:40:55 -0300

    Refatoração do delete time do campeonato.

--- src/controllers/time_campeonatoController.js
+++ src/controllers/time_campeonatoController.js
@@ -1,9 +1,9 @@
-const { Op } = require("sequelize");
+const { Op } = require("sequelize")
 // Importe o modelo de campeonato e de time
-const Time_Campeonato = require("../models/time_campeonato");
-const Campeonato = require("../models/campeonato");
-const Time = require("../models/time");
-const Usuario = require("../models/usuario");
+const Time_Campeonato = require("../models/time_campeonato")
+const Campeonato = require("../models/campeonato")
+const Time = require("../models/time")
+const Usuario = require("../models/usuario")
 
 // Função que retorna os campeonatos existentes
 exports.getAllCampeonato = async (req, res) => {
@@ -12,74 +12,74 @@ exports.getAllCampeonato = async (req, res) => {
 		const campeonatos = await Campeonato.findAll({
 			include: [
 				{ model: Usuario, attributes: ["nomeUsuario"] },
-				{
-					model: Time,
-					attributes: ["idTime", "nomeTime"],
-					through: { attributes: [] }, //remover "Time_Campeonato": {} da resposta de json, são dados repetidos sem necessidade de retorno.
-				},
-			],
-		});
+				{ model: Time, attributes: ["idTime", "nomeTime"],
+				through: { attributes: [] }} //remover "Time_Campeonato": {} da resposta de json, são dados repetidos sem necessidade de retorno.
+			]
+		})
 		// Retorne uma resposta com os campeonatos encontrados
-		return res.status(200).json(campeonatos);
+		return res.status(200).json(campeonatos)
 	} catch (error) {
-		console.error(error);
-		return res.status(500).json({ message: "Erro ao buscar campeonatos" });
+		console.error(error)
+		return res.status(500).json({ message: "Erro ao buscar campeonatos" })
 	}
-};
-
+}
 // Função que vincula os times ao campeonato
 exports.enrollTimesCampeonato = async (req, res) => {
 	try {
-		const { idCampeonato, idTimes } = req.body;
+		const { idCampeonato, idTimes } = req.body
 		// Verifique se o campeonato existe
-		const campeonato = await Campeonato.findByPk(idCampeonato);
+		const campeonato = await Campeonato.findByPk(idCampeonato)
 		if (!campeonato) {
-			return res.status(404).json({ error: "Campeonato não encontrado." });
+			return res.status(404).json({ error: "Campeonato não encontrado." })
 		}
         // Operador ternário para verificar se é um array(matriz) ou não
         const query = Array.isArray(idTimes)
             ? { idTime: { [Op.in]: idTimes } }
-            : { idTime: idTimes };
+            : { idTime: idTimes }
 
-        const times = await Time.findAll({ where: query });
+        const times = await Time.findAll({ where: query })
 		if (times.length == 0) {
-			return res.status(400).json({ error: "Time(s) não encontrado(s)!." });
+			return res.status(400).json({ error: "Time(s) não encontrado(s)!." })
 		}
-
-		// Função que verifica se os times já pertencem ao campeonato
-        const timesCamp = await Time_Campeonato.findAll({ where: query });
+		// Verifica se os times já pertencem ao campeonato
+        const timesCamp = await Time_Campeonato.findAll({ where: query })
 		if (timesCamp.length > 0) {
-			return res.status(400).json({ error: "Time(s) já vinculado(s) ao campeonato." });
+			return res.status(400).json({ error: "Time(s) já vinculado(s) ao campeonato." })
 		}
 		// Associa os times ao campeonato
-		await campeonato.addTimes(times);
+		await campeonato.addTimes(times)
 
-		res.status(200).json({ message: "Time(s) associados ao campeonato com sucesso." });
+		res.status(200).json({ message: "Time(s) associados ao campeonato com sucesso." })
 	} catch (error) {
-		console.error("Erro ao associar times ao campeonato:", error);
-		res.status(500).json({ error: "Erro interno do servidor." });
+		console.error("Erro ao associar times ao campeonato:", error)
+		res.status(500).json({ error: "Erro interno do servidor." })
 	}
-};
-//delete time do campeonato
+}
+// Delete time do campeonato
 exports.deleteTimeCampeonato = async (req, res) => {
 	try {
-		const { idCampeonato, idTime } = req.body;
+		const { idCampeonato, idTimes } = req.body
 		// Verifique se o campeonato existe
-		const campeonato = await Campeonato.findByPk(idCampeonato);
+		const campeonato = await Campeonato.findByPk(idCampeonato)
 		if (!campeonato) {
-			return res.status(404).json({ error: "Campeonato não encontrado." });
+			return res.status(404).json({ error: "Campeonato não encontrado." })
 		}
-		// Verifique se os times existem
-		const time = await Time.findByPk(idTime);
-		if (!time) {
-			return res.status(400).json({ error: "Time não encontrado." });
-		}
-		// Desassocia os times ao campeonato
-		await campeonato.removeTime(time);
+		// Operador ternário para verificar se é um array(matriz) ou não
+        const query = Array.isArray(idTimes)
+            ? { idTime: { [Op.in]: idTimes } }
+            : { idTime: idTimes }
+        // Verifique se os times pertencem ao campeonato
+        const timesCamp = await Time_Campeonato.findAll({ where: query })
+		if (timesCamp.length > 0) {
+			// Desassocia os times ao campeonato
+            await campeonato.removeTime(timesCamp)
+		}else{
+            return res.status(400).json({ error: "Time(s) não vinculado(s) ao campeonato." })
+        }
 
-		res.status(200).json({ message: "Time desassociado ao campeonato com sucesso." });
+		res.status(200).json({ message: "Time(s) desassociado(s) ao campeonato com sucesso." })
 	} catch (error) {
-		console.error("Erro ao desassociar time ao campeonato:", error);
-		res.status(500).json({ error: "Erro interno do servidor." });
+		console.error("Erro ao desassociar time ao campeonato:", error)
+		res.status(500).json({ error: "Erro interno do servidor." })
 	}
-};
+}
