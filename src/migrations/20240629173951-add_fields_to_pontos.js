79d626a1f2f41110a8e305eb25a9bf72d0138b01
Everton Silva <evertonfernandes097@gmail.com>
2024-06-30 10:32:43 -0300

    Feat: Criado migração DB.

---
+++ src/migrations/20240629173951-add_fields_to_pontos.js
@@ -0,0 +1,24 @@
+'use strict';
+/* npx sequelize-cli db:migrate */
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+      await queryInterface.addColumn('pontos', 'ladoQuadraTime1', {
+          type: Sequelize.ENUM('Esquerda', 'Direita'),
+          allowNull: true
+      });
+      await queryInterface.addColumn('pontos', 'ladoQuadraTime2', {
+          type: Sequelize.ENUM('Esquerda', 'Direita'),
+          allowNull: true
+      });
+      await queryInterface.addColumn('pontos', 'saqueInicial', {
+          type: Sequelize.INTEGER,
+          allowNull: true
+      });
+  },
+  down: async (queryInterface, Sequelize) => {
+      await queryInterface.removeColumn('pontos', 'ladoQuadraTime1');
+      await queryInterface.removeColumn('pontos', 'ladoQuadraTime2');
+      await queryInterface.removeColumn('pontos', 'saqueInicial');
+  }
+};
